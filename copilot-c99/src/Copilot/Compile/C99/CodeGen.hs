Copilot-Language/copilot

Add callstacks to Stream constructors

copilot-c99/src/Copilot/Compile/C99/CodeGen.hs
@@ -116,7 +116,7 @@ mkstep cSettings streams triggers exts =
 
     -- Write code to update global stream buffers and index.
     mkupdateglobals :: Stream -> (C.Decln, C.Stmt, C.Stmt, C.Stmt)
-    mkupdateglobals (Stream sid buff expr ty) =
+    mkupdateglobals (Stream sid buff expr ty _) =
       (tmpdecln, tmpassign, bufferupdate, indexupdate)
         where
           tmpdecln = C.VarDecln Nothing cty tmp_var Nothing
@@ -284,7 +284,7 @@ gatherexprs :: [Stream] -> [Trigger] -> [UExpr]
 gatherexprs streams triggers =  map streamexpr streams
                              ++ concatMap triggerexpr triggers
   where
-    streamexpr  (Stream _ _ expr ty)   = UExpr ty expr
+    streamexpr  (Stream _ _ expr ty _)   = UExpr ty expr
     triggerexpr (Trigger _ guard args _) = UExpr Bool guard : args
 
 -- * Auxiliary functions

copilot-c99/src/Copilot/Compile/C99/Compile.hs
@@ -90,8 +90,8 @@ compilec cSettings spec = C.TransUnit declns funs
     mkglobals :: [Stream] -> [C.Decln]
     mkglobals streams = map buffdecln streams ++ map indexdecln streams
       where
-        buffdecln  (Stream sid buff _ ty) = mkbuffdecln  sid ty buff
-        indexdecln (Stream sid _    _ _ ) = mkindexdecln sid
+        buffdecln  (Stream sid buff _ ty _) = mkbuffdecln  sid ty buff
+        indexdecln (Stream sid _    _ _ _) = mkindexdecln sid
 
     -- Make generator functions, including trigger arguments.
     genfuns :: [Stream] -> [Trigger] -> [C.FunDef]
@@ -101,12 +101,12 @@ compilec cSettings spec = C.TransUnit declns funs
       where
 
         accessdecln :: Stream -> C.FunDef
-        accessdecln (Stream sid buff _ ty) = mkaccessdecln sid ty buff
+        accessdecln (Stream sid buff _ ty _) = mkaccessdecln sid ty buff
 
         streamgen :: Stream -> C.FunDef
-        streamgen (Stream sid _ expr ty@(Array _)) =
+        streamgen (Stream sid _ expr ty@(Array _) _) =
           genFunArray (generatorname sid) (generatorOutputArgName sid) expr ty
-        streamgen (Stream sid _ expr ty) = genfun (generatorname sid) expr ty
+        streamgen (Stream sid _ expr ty _) = genfun (generatorname sid) expr ty
 
         triggergen :: Trigger -> [C.FunDef]
         triggergen (Trigger name guard args _) = guarddef : argdefs

GaloisInc/copilot-1

copilot-c99/src/Copilot/Compile/C99/CodeGen.hs
@@ -185,7 +185,7 @@ mkStep cSettings streams triggers exts =
 
     -- Write code to update global stream buffers and index.
     mkUpdateGlobals :: Stream -> (C.Decln, C.Stmt, C.Stmt, C.Stmt)
-    mkUpdateGlobals (Stream sId buff _expr ty) =
+    mkUpdateGlobals (Stream sId buff _expr ty _) =
       (tmpDecln, tmpAssign, bufferUpdate, indexUpdate)
         where
           tmpDecln = C.VarDecln Nothing cTy tmpVar Nothing

copilot-c99/src/Copilot/Compile/C99/Compile.hs
@@ -106,8 +106,8 @@ compileC cSettings spec = C.TransUnit declns funs
     mkGlobals streamList =  map buffDecln streamList
                          ++ map indexDecln streamList
       where
-        buffDecln  (Stream sId buff _ ty) = mkBuffDecln  sId ty buff
-        indexDecln (Stream sId _    _ _ ) = mkIndexDecln sId
+        buffDecln  (Stream sId buff _ ty _) = mkBuffDecln  sId ty buff
+        indexDecln (Stream sId _    _ _  _) = mkIndexDecln sId
 
     -- Make generator functions, including trigger arguments.
     mkGenFuns :: [Stream] -> [Trigger] -> [C.FunDef]
@@ -116,10 +116,10 @@ compileC cSettings spec = C.TransUnit declns funs
                                      ++ concatMap triggerGen triggerList
       where
         accessDecln :: Stream -> C.FunDef
-        accessDecln (Stream sId buff _ ty) = mkAccessDecln sId ty buff
+        accessDecln (Stream sId buff _ ty _) = mkAccessDecln sId ty buff
 
         streamGen :: Stream -> C.FunDef
-        streamGen (Stream sId _ expr ty) =
+        streamGen (Stream sId _ expr ty _) =
           exprGen (generatorName sId) (generatorOutputArgName sId) expr ty
 
         triggerGen :: Trigger -> [C.FunDef]
@@ -254,5 +254,5 @@ gatherExprs :: [Stream] -> [Trigger] -> [UExpr]
 gatherExprs streams triggers =  map streamUExpr streams
                              ++ concatMap triggerUExpr triggers
   where
-    streamUExpr  (Stream _ _ expr ty)   = UExpr ty expr
+    streamUExpr  (Stream _ _ expr ty _)   = UExpr ty expr
     triggerUExpr (Trigger _ guard args _) = UExpr Bool guard : args
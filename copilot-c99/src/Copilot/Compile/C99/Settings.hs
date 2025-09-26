5ac4fbf0e68e9cb8b1eea72d9c04ac3a666a1901
Trevor Kann <tkann@galois.com>
2025-09-26 14:22:54 -0400

    attempting a new method for choosing the spec creation location

--- copilot-c99/src/Copilot/Compile/C99/Settings.hs
+++ copilot-c99/src/Copilot/Compile/C99/Settings.hs
@@ -1,9 +1,26 @@
+{-# LANGUAGE CPP #-}
 -- | Settings used by the code generator to customize the code.
 module Copilot.Compile.C99.Settings
     ( CSettings(..)
     , mkDefaultCSettings
+    , OutputDirectory
+    , outputDirectoryToPath
     )
   where
+
+import System.FilePath ( takeDirectory )
+
+-- | Enumerator to show where the generated code should be placed
+data OutputDirectory = WithSpec 
+                      | CurrentDir
+                      | Custom String
+                      deriving (Show, Eq)
+
+outputDirectoryToPath :: OutputDirectory -> IO FilePath
+outputDirectoryToPath (Custom dir) = return dir
+outputDirectoryToPath CurrentDir = return "."
+outputDirectoryToPath WithSpec = return (takeDirectory __FILE__)
+
 
 -- | Settings used to customize the code generated.
 data CSettings = CSettings
